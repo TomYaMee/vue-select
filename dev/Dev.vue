7aef19383e6c750e884eb2b6af7a9e348e66fb71
Tommy Ang <tommy9611@gmail.com>
2021-05-21 15:22:40 +0800

    fix: height of dropdown not dynamic

--- dev/Dev.vue
+++ dev/Dev.vue
@@ -2,7 +2,30 @@
   <div id="app">
     <v-select v-model="selected" v-bind="config" />
     <v-select v-model="selected" v-bind="config">
-      <template #sticky-footer><div>Test1223</div></template>
+      <template #sticky-footer>
+        <b-row align-v="center">
+          <b-col cols="12" md="7">
+            <slot name="footer-template">
+              <div class="title">Can't find your practice?</div>
+              <div class="subtitle">
+                If you can't find a practice in the list you can add it manually
+              </div>
+            </slot>
+          </b-col>
+          <b-col cols="12" md="5" class="text-right">
+            <BaseButton
+              class="font-weight-bold ml-auto"
+              :icon="['fas', 'plus']"
+              :variant="variant"
+              @click="$emit('add')"
+            >
+              <div style="font-size: 18px" class="m-0">
+                <slot name="footer-button">Add Manually</slot>
+              </div>
+            </BaseButton>
+          </b-col>
+        </b-row>
+      </template>
     </v-select>
   </div>
 </template>
